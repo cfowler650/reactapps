e8e8797cdab47bea0c48e8212580b83a0728d33d
cfowler650 <cfowler650@gmail.com>
2019-07-23 18:00:01 -0700

    Updated Assignment 1 completed

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class DishDetail extends Component {
+      constructor(props) {
+        super(props)
+      }
+
+    renderDish(dish) {
+      if (dish != null) {
+        return (
+          <div className="col-md-5 ml-1">
+            <Card>
+              <CardImg width="100%" object src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle heading>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+
+            </Card>
+          </div>
+        );
+      }
+      else {
+        return (
+          <div></div>
+        )
+      }
+    }
+
+
+    renderComments(dish) {
+      if (dish != null) {
+        return (
+          <div className="col-12 col-md-5 ml-2">
+            <Card style={{border: "none"}}>
+              <CardImg width="100%" />
+              <CardBody>
+                <CardTitle heading><h2>Comments</h2></CardTitle>
+                <CardText>
+                {
+                  dish.comments.map(commentObj => {
+                    return(
+                      <ul className="list-unstyled">
+                        <li className="list-unstyled mt-2 mb-2">
+
+                        { commentObj.comment }
+
+                        </li>
+
+                        <li className="list-unstyled mt-2 mb-2">
+                         -- { commentObj.author}, { commentObj.date }
+                        </li>
+
+                      </ul>
+                    )
+                  }
+                  )
+                }
+                </CardText>
+              </CardBody>
+
+            </Card>
+        </div>
+        )
+      } else {
+        return (
+          <div></div>
+        )
+      }
+    }
+
+
+
+    render() {
+      return(
+          <div className="row">
+            { this.renderDish(this.props.dish) }
+            { this.renderComments(this.props.dish) }
+         </div>
+        )
+      }
+  }
+
+export default DishDetail;
