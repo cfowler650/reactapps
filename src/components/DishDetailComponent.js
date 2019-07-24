00399758f65ab91e6302aec19c5e3e4963152fc7
cfowler650 <cfowler650@gmail.com>
2019-07-24 09:14:09 -0700

    functional components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,36 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
+  // {} extracts that specific
+  function RenderDish( {dish} ) {
+        if (dish != null) {
+          return (
+            <div className="col-md-5 ml-1">
+              <Card>
+                <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle heading>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
 
-class DishDetail extends Component {
-      constructor(props) {
-        super(props)
-      }
-
-    renderDish(dish) {
-      if (dish != null) {
-        return (
-          <div className="col-md-5 ml-1">
-            <Card>
-              <CardImg width="100%" object src={dish.image} alt={dish.name} />
-              <CardBody>
-                <CardTitle heading>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-
-            </Card>
-          </div>
-        );
-      }
-      else {
-        return (
-          <div></div>
-        )
-      }
+              </Card>
+            </div>
+          );
+        }
+        else {
+          return (
+            <div></div>
+          )
+        }
     }
 
 
-    renderComments(dish) {
+  function RenderComments( {dish} ) {
       if (dish != null) {
         return (
           <div className="col-12 col-md-5 ml-2">
@@ -43,7 +38,7 @@ class DishDetail extends Component {
                   dish.comments.map(commentObj => {
                     return(
                       <ul className="list-unstyled">
-                        <li className="list-unstyled mt-2 mb-2">
+                        <li key={commentObj.id} className="list-unstyled mt-2 mb-2">
 
                         { commentObj.comment }
 
@@ -73,16 +68,16 @@ class DishDetail extends Component {
 
 
 
-    render() {
+    const DishDetail = (props) =>  {
       return(
         <div className="container">
           <div className="row">
-            { this.renderDish(this.props.dish) }
-            { this.renderComments(this.props.dish) }
-         </div>
+          <RenderDish dish={props.dish} />
+            {/* { this.renderComments(this.props.dish) } */}
+          <RenderComments dish={props.dish} />
+          </div>
         </div>
         )
       }
-  }
 
 export default DishDetail;
