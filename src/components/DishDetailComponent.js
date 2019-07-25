3988e41cd810006eeab7eb860e57fd253a855385
cfowler650 <cfowler650@gmail.com>
2019-07-25 10:36:00 -0700

    SPA part 2 added breadcrumbs

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
   // {} extracts that specific
   function RenderDish( {dish} ) {
@@ -24,40 +25,39 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
         }
     }
 
-
-  function RenderComments( {dish} ) {
-      if (dish != null) {
+    function RenderComments({ comments }) {
+      if (comments != null) {
         return (
           <div className="col-12 col-md-5 ml-2">
-            <Card style={{border: "none"}}>
+            <Card style={{ border: "none" }}>
               <CardImg width="100%" />
               <CardBody>
                 <CardTitle heading><h2>Comments</h2></CardTitle>
                 <CardText>
-                {
-                  dish.comments.map(commentObj => {
-                    return(
-                      <ul className="list-unstyled">
-                        <li key={commentObj.id} className="list-unstyled mt-2 mb-2">
+                  {
+                    comments.map(commentObj => {
+                      return (
+                        <ul className="list-unstyled">
+                          <li key={commentObj.id} className="list-unstyled mt-2 mb-2">
 
-                        { commentObj.comment }
+                            {commentObj.comment}
 
-                        </li>
+                          </li>
 
-                        <li className="list-unstyled mt-2 mb-2">
-                         -- { commentObj.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(commentObj.date)))}
-                        </li>
+                          <li className="list-unstyled mt-2 mb-2">
+                            -- {commentObj.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(commentObj.date)))}
+                          </li>
 
-                      </ul>
+                        </ul>
+                      )
+                    }
                     )
                   }
-                  )
-                }
                 </CardText>
               </CardBody>
 
             </Card>
-        </div>
+          </div>
         )
       } else {
         return (
@@ -69,12 +69,24 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
 
     const DishDetail = (props) =>  {
+
       return(
+
         <div className="container">
           <div className="row">
-          <RenderDish dish={props.dish} />
-            {/* { this.renderComments(this.props.dish) } */}
-          <RenderComments dish={props.dish} />
+            <Breadcrumb>
+              <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr />
+            </div>
+          </div>
+
+          <div className="row">
+            <RenderDish dish={props.dish} />
+            <RenderComments comments={props.comments}/>
           </div>
         </div>
         )
